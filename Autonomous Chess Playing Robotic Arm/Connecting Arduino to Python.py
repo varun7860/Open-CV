8713ff192cf6ee729145256efcacc8dbb8c05b74
Varun walimbe <51876661+varun7860@users.noreply.github.com>
2020-05-27 16:03:43 +0530

    Update Connecting Arduino to Python.py

--- Autonomous Chess Playing Robotic Arm/Connecting Arduino to Python.py
+++ Autonomous Chess Playing Robotic Arm/Connecting Arduino to Python.py
@@ -1,1 +1,9 @@
+import pyfirmata
 
+board = pyfirmata.Arduino('com8')
+servo_pin1 = board.get_pin('d:Pin Number:s')
+servo_pin2 = board.get_pin('d:Pin Number:s')
+servo_pin3 = board.get_pin('d:Pin Number:s')
+servo_pin4 = board.get_pin('d:Pin Number:s')
+
+'''  here "d" means digital pin . you can also use "a" for analog pin '''
